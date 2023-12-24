f447a118b21ad3ef68cebb03dc2dce6d7e3c5b01
tyange <usun16@gmail.com>
2023-12-24 21:40:34 +0900

    add test for multiple expectation

--- src/util/numbers.test.js
+++ src/util/numbers.test.js
@@ -10,10 +10,21 @@ it("should transform a string number to a number of type number", () => {
   expect(result).toBeTypeOf("number");
 });
 
-it("should yield NaN for non-transformable values", () => {
-  const input = "invalid";
+it("should transform a string number to a number of type number", () => {
+  const input = "1";
 
   const result = transformToNumber(input);
 
+  expect(result).toBe(+input);
+});
+
+it("should yield NaN for non-transformable values", () => {
+  const input = "invalid";
+  const input2 = {};
+
+  const result = transformToNumber(input);
+  const result2 = transformToNumber(input2);
+
   expect(result).toBeNaN();
+  expect(result2).toBeNaN();
 });
