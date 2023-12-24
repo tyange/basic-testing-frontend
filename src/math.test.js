fcfd2a6ca29fe90458296ce5ad0e7a4f7a930377
tyange <usun16@gmail.com>
2023-12-24 21:06:37 +0900

    detecting problem with test

--- src/math.test.js
+++ src/math.test.js
@@ -16,3 +16,23 @@ it("should summarize all number values in an array", () => {
 
   expect(result).toBe(expectedResult);
 });
+
+it("should yield NaN if a least one invalid number is provided", () => {
+  const inputs = ["invalid", 1];
+
+  const result = add(inputs);
+
+  expect(result).toBeNaN();
+});
+
+it("should yield a correct sum if an array of numeric string values is provided", () => {
+  const numbers = ["1", "2"];
+
+  const result = add(numbers);
+
+  const expectedResult = numbers.reduce(
+    (prevValue, curValue) => +prevValue + +curValue,
+    0
+  );
+  expect(result).toBe(expectedResult);
+});
