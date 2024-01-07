b16ae7442d2cad870254a90b8ed2bc7b47bce1a2
tyange <usun16@gmail.com>
2024-01-08 06:37:18 +0900

    using toEqual

--- src/util/numbers.test.js
+++ src/util/numbers.test.js
@@ -37,7 +37,8 @@ describe("cleanNumbers()", () => {
 
     const cleanedNumbers = cleanNumbers(numberValues);
 
-    expect(cleanedNumbers[0]).toBeTypeOf("number");
+    // expect(cleanedNumbers[0]).toBeTypeOf("number");
+    expect(cleanedNumbers).toEqual([1, 2]);
   });
 
   it("should throw an error if an array with at least one empty string is provided", () => {
